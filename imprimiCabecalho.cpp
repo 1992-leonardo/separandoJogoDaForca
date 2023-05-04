dd061be32b981aaf6e121282fd49f5d49dee2ac8
Leonardo Possan <leonardopossamdasilva@gmail.com>
2023-05-04 20:41:10 -0300

    So pra ver se builda

--- imprimiCabecalho.cpp
+++ imprimiCabecalho.cpp
@@ -5,5 +5,7 @@ void imprimiCabecalho(){
     std::cout << "**************************************" << std::endl;
     std::cout << "* Bem-vindos ao jogo da adivinhacao do LP! *" << std::endl;
     std::cout << "**************************************" << std::endl;
+
+    std::cout << "So pra ver se builda" << std::endl;
     
 }
