ba66cc0f6e3ddf4318d3e163737743c05596c9a8
Leonardo Possan <66965034+1992-leonardo@users.noreply.github.com>
2023-05-04 20:43:11 -0300

    Update imprimiCabecalho.cpp

--- imprimiCabecalho.cpp
+++ imprimiCabecalho.cpp
@@ -5,7 +5,5 @@ void imprimiCabecalho(){
     std::cout << "**************************************" << std::endl;
     std::cout << "* Bem-vindos ao jogo da adivinhacao do LP! *" << std::endl;
     std::cout << "**************************************" << std::endl;
-
-    std::cout << "So pra ver se builda" << std::endl;
-    
+        
 }
